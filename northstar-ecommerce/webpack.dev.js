2c13a05c566ebfc37ecb54c67b6b8d63be3bbd88
anujshivapatel <anupatel@adobe.com>
2023-03-31 20:21:14 +0530

    Fixes and improvements

--- northstar-ecommerce/webpack.dev.js
+++ northstar-ecommerce/webpack.dev.js
@@ -2,18 +2,12 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const buildPath = path.resolve(__dirname, 'dist');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-//const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
 
-  // https://webpack.js.org/configuration/mode/
   mode: 'development',
-
-  // This option controls if and how source maps are generated.
-  // https://webpack.js.org/configuration/devtool/
   devtool: 'eval-cheap-module-source-map',
 
-  // https://webpack.js.org/concepts/entry-points/#multi-page-application
   entry: {
     index: './src/js/index.js',
   },
@@ -22,17 +16,13 @@ module.exports = {
     path: buildPath,
     clean: true
   },
-  // https://webpack.js.org/configuration/dev-server/
   devServer: {
-    port: 3002
-    //writeToDisk: false  https://webpack.js.org/configuration/dev-server/#devserverwritetodisk-
+    port: 3000
   },
 
-  // https://webpack.js.org/concepts/loaders/
   module: {
     rules: [
       {
-        // https://webpack.js.org/loaders/babel-loader/#root
         test: /\.m?js$/i,
         exclude: /node_modules/,
         use: {
@@ -40,41 +30,20 @@ module.exports = {
           options: {
             presets: ['@babel/preset-env']
           }
-        // options: {
-        //    presets: [
-        //      ['@babel/preset-env', { targets: "defaults" }]
-        //    ]
-        //  }
         }
       },
       {
-        // https://webpack.js.org/loaders/sass-loader/
         test: /\.s[ac]ss$/i,
         use: [
-          // Creates `style` nodes from JS strings
           "style-loader",
-          // Translates CSS into CommonJS
           "css-loader",
-          // Compiles Sass to CSS
           "sass-loader",
         ]
       },
-      /*{
-        test: /\.(gif|png|avif|jpe?g)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'assets/images/'
-            }
-          }
-        ]
-      }*/
+      
     ]
   },
 
-  // https://webpack.js.org/concepts/plugins/
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
@@ -92,21 +61,12 @@ module.exports = {
       template: './src/html/contactUs.html',
       inject: true,
       chunks: ['index'],
-      filename: 'contact-us.html'
+      filename: 'contactUs.html'
     }),
     new CopyWebpackPlugin({
       patterns: [
         {from:'src/resources',to:'assets/images/'} 
-        //{ from: "other", to: "public" },
       ],
-    }),/*,
-    new webpack.ProvidePlugin({
-      $: require.resolve('jquery'),
-      jQuery: require.resolve('jquery')
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].[contenthash].css',
-      chunkFilename: '[id].[contenthash].css'
-    })*/
+    })
   ]
 }
